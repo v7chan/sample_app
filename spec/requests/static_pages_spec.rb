4dabfb20523a115b2fd793face80c01264bd1a1e
Vincent Chan <v7chan@gmail.com>
2014-02-23 22:31:47 -0800

    Add Contact Page and clean up static pages spec

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,11 +1,12 @@
 require 'spec_helper'
 
 describe "Static Pages" do
+  let(:base_title) { "Ruby on Rails Tutorial Sample App" }
 
   describe "Home Page" do
     it "should have the right title" do
       visit '/static_pages/home'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+      expect(page).to have_title("#{base_title} | Home")
     end
 
     it "should have the content 'Sample App'" do
@@ -17,7 +18,7 @@ describe "Static Pages" do
   describe "Help Page" do
     it "should have the right title" do
       visit '/static_pages/help'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+      expect(page).to have_title("#{base_title} | Help")
     end
 
     it "should have the content 'Help'" do
@@ -29,12 +30,24 @@ describe "Static Pages" do
   describe "About Page" do
     it "should have the right title" do
       visit '/static_pages/about'
-      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+      expect(page).to have_title("#{base_title} | About Us")
     end
 
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
+    end
+  end
+
+  describe "Contact Page" do
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("#{base_title} | Contact")
+    end
+
+    it "should have the content 'Contact'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact')
     end
   end
 end
