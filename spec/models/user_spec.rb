0efa4b6185171cf40d750abf08c932c2cccd32fc
Vincent Chan <v7chan@gmail.com>
2014-03-09 22:14:06 -0700

    Improve regex to catch foo@bar..com and add test for downcase before save

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -31,7 +31,7 @@ describe User do
 
   describe "when email format is invalid" do
     it "should be invalid" do
-      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+      addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -58,6 +58,17 @@ describe User do
 
     it { should_not be_valid }
   end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "fOo@ExAmPle.bar" }
+
+    it "should save the email with downcase" do 
+      @user.email = mixed_case_email
+      @user.save
+
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end  
 
   describe "when password is not present" do
     before do
